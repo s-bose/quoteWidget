d0fa16097fa3fa03d7cf5faa050f404c822a04fd
Shiladitya Basu <shiladitya.31.z@gmail.com>
2019-10-16 19:23:39 +0530

    added custom title bar and draggable

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-
-
-
-const request = require('request');
-
-
+//const request = require('request');
+const remote = require('electron').remote;
 console.log('index.js is alive and running')
 
+
 $('document').ready(() => {
+
+    function customNavbar() {
+        var win = remote.getCurrentWindow();
+
+        // var title = document.querySelector("title").innerHTML;
+        // document.querySelector("#titleshown").innerHTML = title;
+
+        var minimize = document.querySelector("#minimize");
+        var quit = document.querySelector("#quit");
+
+        minimize.addEventListener("click", event => {
+            win = remote.getCurrentWindow();
+            win.minimize();
+        });
+
+        quit.addEventListener("click", event => {
+            win = remote.getCurrentWindow();
+
+            win.close();
+        });
+    };
+
     //remember, 10 is the magic number, there are 10 indexes in the directory
-    //10 quotes in each one. 
+    //10 quotes in each one.
 
     //generates random quotes and modifies h1 tag
-    function generateQuote(){
+    function generateQuote() {
         let randomPage = Math.floor(Math.random() * 10);
 
         let quoteUrl = "http://quotes.toscrape.com/";
         quoteUrl = quoteUrl + "/page/" + randomPage;
-    
+
         const MAX_CHAR = 256;
-    
-        $.get(quoteUrl, function(response) {
+
+        $.get(quoteUrl, function (response) {
             let quotes = $(response).find(".quote");
             let qs = [];
-            quotes.each(function(i, quote){
+            quotes.each(function (i, quote) {
                 let text = $(quote).find('.text').html();
                 let author = $(quote).find('.author').html();
-                qs.push({"writer": author, "quote": text});
+                qs.push({
+                    "writer": author,
+                    "quote": text
+                });
             })
 
-            qs = qs.filter(function(entries){
+            qs = qs.filter(function (entries) {
                 if (entries.quote.length <= MAX_CHAR)
                     return entries;
             })
@@ -46,14 +68,16 @@ $('document').ready(() => {
 
             $('h1').text(randomQuote);
             $('p').text(randomAuthor);
-        
+
         })
     }
 
+    customNavbar();
     generateQuote();
 
     myUrl = 'https://www.picsum.photos/1000/1000?blur=2'
-    function changeUrl(){
+
+    function changeUrl() {
         let currTime = new Date().getTime()
         let newUrl = myUrl + "?" + currTime
         generateQuote();
